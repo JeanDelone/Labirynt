38f8506f5ce5fa272c96c9389af1a85f676f35ac
Jan Dołbniak <jd@MacBook-Air-Jan.local>
2022-06-21 10:34:16 +0200

    main cleared

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,6 @@ public class Main {
         int columns = 10;
 
         Maze maze2 = new Maze(rows, columns);
-//        System.out.println(maze2.maze[0][1]);
-//        System.out.println(" ");
-//        System.out.println(maze2.maze[0][1].edgesTo);
-//        System.out.println(" ");
-//        for (int i = 0; i < columns; i++) {
-//            for (int j = 0; j < rows; j++) {
-//                System.out.println(maze2.maze[i][j] + "             " + maze2.maze[i][j].edgesTo);
-//
-//            }
-//            System.out.println(" ");
-//
-//        }
 
         maze2.drawMaze();
         maze2.save("Labirynt1");
